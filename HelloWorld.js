00bcf146857652792d741ec843b758abca3b13eb
tjw84 <way16006@byui.edu>
2023-04-22 23:39:58 -0600

    Add files via upload
    Added my severally over coded js and html files.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+
+document.querySelector('#hello').addEventListener('click', hello);
+
+function hello() {
+    console.log("Hello World!");
+    return "Hello World!";
+  
+  }
